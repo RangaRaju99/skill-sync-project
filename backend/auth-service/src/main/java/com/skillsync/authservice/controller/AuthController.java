da4409912dfe84b41ee77dbd6b0f38c0a7a77147
Bathula Rangaraju <rangafree1@gmail.com>
2026-04-11 16:01:44 +0530

    tested working

--- backend/auth-service/src/main/java/com/skillsync/authservice/controller/AuthController.java
+++ backend/auth-service/src/main/java/com/skillsync/authservice/controller/AuthController.java
@@ -100,7 +100,7 @@ public class AuthController {
     }
 
     // ─────────────────────────────────────────────────────────────
-    // Forgot Password
+    // Forgot Password --i think
     // ─────────────────────────────────────────────────────────────
 
     @PostMapping("/forgot-password")
